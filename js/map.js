5f72f200af1abff72bbc84eb01500790f4b7fe35
Jesse <jesse.xu.20@gmail.com>
2015-10-11 01:28:53 -0400

    revert back to angular js

--- js/map.js
+++ js/map.js
@@ -1,39 +1,39 @@
 //Data
-var cities = [
+var events = [
     {
-        city : 'Toronto',
-        desc : 'This is the best city in the world!',
+        event : 'Toronto',
+        desc : 'This is the best event in the world!',
         lat : 43.7000,
         long : -79.4000
     },
     {
-        city : 'New York',
-        desc : 'This city is aiiiiite!',
+        event : 'New York',
+        desc : 'This event is aiiiiite!',
         lat : 40.6700,
         long : -73.9400
     },
     {
-        city : 'Chicago',
-        desc : 'This is the second best city in the world!',
+        event : 'Chicago',
+        desc : 'This is the second best event in the world!',
         lat : 41.8819,
         long : -87.6278
     },
     {
-        city : 'Los Angeles',
-        desc : 'This city is live!',
+        event : 'Los Angeles',
+        desc : 'This event is live!',
         lat : 34.0500,
         long : -118.2500
     },
     {
-        city : 'Las Vegas',
-        desc : 'Sin City...\'nuff said!',
+        event : 'Las Vegas',
+        desc : 'Sin event...\'nuff said!',
         lat : 36.0800,
         long : -115.1522
     }
 ];
 
 
-function myFunction(arr)
+function myEvents(arr)
 {
     var xmlhttp = new XMLHttpRequest();
     var url = "photo/json";
@@ -49,65 +49,62 @@ function myFunction(arr)
     xmlhttp.send();
 }
 
-
-
-var xmlhttp = new XMLHttpRequest();
-var url = "photo/json";
-var events;
-
-xmlhttp.onreadystatechange = function() {
-
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        console.log("true")
-        var myArr = JSON.parse(xmlhttp.responseText);
-        events= myArr["data"]
-        console.log(events.length)
-        myFunction2(myArr["data"])
-
-    }
-}
-console.log(events)
-
 var map = {};
+var redDot = new google.maps.MarkerImage(
+    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|C92A2A",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new google.maps.Size(20, 34)
+);
+var blueDot = new google.maps.MarkerImage(
+    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2F4AC9",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new google.maps.Size(20, 34)
+); 
+
 
 //Angular App Module and Controller
-var MapModule = angular.module('mapsApp', ['ngRoute']);
+var MapModule = angular.module('mapsApp', []);
 
-console.log(events)
+
 MapModule.controller('MapCtrl', function ($scope) {
-    console.log("false")
+
     var mapOptions = {
         zoom: 4,
-        center: new google.maps.LatLng(35.0, -80.0000),
+        center: new google.maps.LatLng(40.0000, -98.0000),
         // mapTypeId: google.maps.MapTypeId.TERRAIN
-
+		
     }
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     $scope.markers = [];
-
+    
     var infoWindow = new google.maps.InfoWindow();
-
+    
     var createMarker = function (info){
-
+ 
+        // var image='img/red_marker.png'
         var marker = new google.maps.Marker({
             map: $scope.map,
-            position: new google.maps.LatLng(info["latitude"], info["longitude"]),
-            title: info["name"]
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.event,
+			icon: redDot
         });
-        marker.content = '<div class="infoWindowContent">' + info["description"] + '</div>';
-
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
         google.maps.event.addListener(marker, 'click', function(){
             infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
             infoWindow.open($scope.map, marker);
         });
         $scope.markers.push(marker);
         return marker;
-
-    }
-
-
+        
+    }  
+    
     for (i = 0; i < events.length; i++){
         map[i] = createMarker(events[i]);
     }
@@ -121,47 +118,11 @@ MapModule.controller('MapCtrl', function ($scope) {
 
 MapModule.controller('EventBoxCtrl', function ($scope) {
     $scope.hoverIn = function() {
-        map[1].setIcon("http://www.googlemapsmarkers.com/v1/A/0099FF/");
+        map[1].setIcon(blueDot);
     }
 
     $scope.hoverOut = function() {
-        map[1].setIcon("http://www.googlemapsmarkers.com/v1/A/0099FF/");
+        map[1].setIcon(redDot);
     }
 
 });
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-function myFunction2(events) {
-    var mystr = '<div class="col-md-4 portfolio-item">\
-                            <div class="service-box" ng-mouseover="hoverIn(numberid)" ng-mouseleave="hoverOut(numberid)">\
-                            <a href="#">\
-                            <img class="img-responsive" src="photo/cover/image" alt="">\
-                            </a>\
-                            <h3>projectname</h3>\
-                            <p class="text-muted">description</p>\
-                           </div>\
-                           </div>'
-    var j;
-    var allstr='<div class="col-md-7" ng-controller="EventBoxCtrl">'
-    var currentstr
-    for (j = 0; j < Math.min(events.length, 6); j++) {
-        currentstr = mystr
-        var tempstr = j;
-        tempstr.toString()
-        currentstr = currentstr.replace("numberid", tempstr)
-        currentstr =currentstr.replace("image",tempstr+".jpg")
-        currentstr = currentstr.replace(/projectname/i, events[j]["name"])
-        currentstr = currentstr.replace(/description/i, events[j]["description"].substring(1, 80))
-        allstr +=currentstr
-    }
-    allstr +='<div class="col-md-4">\
-                <div id="map" ng-controller="MapCtrl">\
-               </div>'
-    document.getElementById("rowevents").innerHTML = allstr;
-
-    console.log("1")
-
-}
-
-
