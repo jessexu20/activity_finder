1908f9bf9cf04e81041e8a3c62a23eac37d637e2
Jesse <jesse.xu.20@gmail.com>
2015-10-10 20:21:22 -0400

    modify python

--- photo/get_cover.py
+++ photo/get_cover.py
@@ -3,26 +3,53 @@ import requests
 import os, sys
 import urllib
 from PIL import Image
-access_token="CAACEdEose0cBAPqZBJhlt0IW19ZCMaat4ZAlVV8lJa8PbXDmuYZCMWrTUcDhaNK69jcRAU4cqj0L6Wh7DhKZCARqgxHRx1WdPzq4Fq2x3HuK0bXymCJOEQoal0MRK2SuZAEv0Bz2OM7u8NqcKc0kjWwhIioeaxOzl3sNrrY9e5T6r00AKGDbFmqHZBm7qNC89DEf2me7tvf5SBZASw4XIgUO"
+access_token="CAACEdEose0cBAA6w3tRDSWsr4Vd0VrZBu83niDh2ctsVfZAsT3xj3E5q2hQB73UtxyRgMoiSl75PnB7JVD9LF5VgyVRCPdOhYztAyvFWvkCUDkJBMshgnFdWJwWzK48bl1BswpbVnznau5Ta2hLegqRRAZBalUYEcipCTAjw3rJD3JXn6lfGOUMqEuOHVFmIvQBQe8LfZCc3qiVXrqEw"
 fileR=open("event-id","r")
 fileW=open("json", "w")
 def get_cover():
-	count = 0
-	for line in fileR:
-		url = "https://graph.facebook.com/v2.5/"+line+"?fields=cover&access_token="+access_token
-		r = requests.get(url)
-		if r.status_code == 200:
-			dic=r.json()
-			if dic.has_key('cover'):
-				dic2=dic['cover']
-			if dic2.has_key('source'):
-				download_cover(dic2['source'], str(count)+".jpg")
-				count = count + 1
-				url2 = "https://graph.facebook.com/v2.5/"+line+"?&access_token="+access_token
-				r2=requests.get(url2)
-				if r2.status_code == 200:
-						json.dump(r2.json(), fileW);
-
+    # param={
+    #     "name": "",
+    #     "id":"",
+    #     "description":"",
+    #     "start_time":"",
+    #     "end_time":"",
+    #     "longtitude":0,
+    #     "latitude":0,
+    #     "city":"",
+    #     }
+    # }
+    count = 0
+    for line in fileR:
+        if count > 6:
+            break;
+        url = "https://graph.facebook.com/v2.5/"+line+"?fields=cover&access_token="+access_token#has cover
+        r = requests.get(url)
+        if r.status_code == 200:
+            dic=r.json()
+            if dic.has_key('cover'):
+                dic2=dic['cover']
+                if dic2.has_key('source'):
+                    url2 = "https://graph.facebook.com/v2.5/"+line+"?&access_token="+access_token
+                    r2=requests.get(url2)
+                    if r2.status_code == 200:
+                        eventjson=r2.json();
+                        if eventjson.has_key('place')==False or eventjson.has_key('description')==False or eventjson.has_key('name')==False or eventjson.has_key('start_time')==False or eventjson.has_key('end_time')==False :
+                            continue
+                        placejson=eventjson['place']
+                        if(placejson.has_key('location') and placejson['location'].has_key('longitude') and placejson['location'].has_key('latitude') and placejson['location'].has_key('city')):
+                            download_cover(dic2['source'], str(count)+".jpg")
+                            count = count + 1
+                            param={}
+                            param['name']=eventjson['name']
+                            param['id']=str(count)
+                            param['description']=eventjson['description']
+                            param['start_time']=eventjson['start_time']
+                            param['end_time']=eventjson['end_time']
+                            param['longtitude']=placejson['location']['longitude']
+                            param['latitude']=placejson['location']['latitude']
+                            param['city']=placejson['location']['city']
+                            # print param
+                            json.dump(param, fileW,sort_keys=True);
 def download_cover(url,name):
 	image=urllib.URLopener()
 	image.retrieve(url,"cover/"+name)
